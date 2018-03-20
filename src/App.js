2645fe1688add03bec0b2a2075d2e7dcf277ddb3
Dylanwoo <13128589345@163.com>
2018-03-20 22:48:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'AIzaSyDJO37Cx7EyABWOXVZWDBou-wau3dIsYCQ';
+const webpageTest_API = 'A.42f94b48a17054d80dd1c592a4e2d4d5';
+const URL_TO_GET_RESULTS_FOR = 'http://www.4399.com';
+const API_URL = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?';
+const webpageTest_URL = 'http://www.webpagetest.org/runtest.php?';
+const query = [
+  'url=' + URL_TO_GET_RESULTS_FOR,
+  'key=' + API_KEY,
+  'locale=zh',   //中文zh
+].join('&');
+
+// const query2 = [
+//   'url=' + URL_TO_GET_RESULTS_FOR,
+//   'key=' + webpageTest_API,
+// ]
 class App extends Component {
+
+  state = {
+    image: null
+  }
+  componentDidMount() {
+    //Google pageSpeed API
+    fetch(API_URL+query,{
+      headers: {
+          'Content-type':'application/json',
+          'Accept':'application/json',
+          'cache-control': 'max-age=604800'   //设置浏览器缓存7天
+      }
+    })
+    .then((response) => response.json())
+    .then(result => {
+      console.log(result);
+    })
+
+    //webpageTest API
+    // fetch(webpageTest_URL+query2,{
+    //   headers: {
+    //     'Content-type':'application/json',
+    //     'Accept':'application/json',
+    //   }
+    // })
+    // .then((response) => response.json())
+    // .then(result => {
+    //   console.log(result)
+    // })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Button>hi</Button>
       </div>
     );
