9d21d5089e9e5ef6c3b6079181b9833a90f13e8b
wufeng6 <wufeng6@jd.com>
2018-03-23 20:53:04 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import './App.less';
 
+/**
+ * webpageTest API
+ * 请求方式：http://www.webpagetest.org/runtest.php?url=目标网址&k=api_key
+ */
 const API_KEY = 'AIzaSyDJO37Cx7EyABWOXVZWDBou-wau3dIsYCQ';
 const webpageTest_API = 'A.42f94b48a17054d80dd1c592a4e2d4d5';
 const URL_TO_GET_RESULTS_FOR = 'http://www.4399.com';
@@ -14,10 +18,11 @@ const query = [
 ].join('&');
 const { Header, Sider, Content } = Layout;
 
-// const query2 = [
-//   'url=' + URL_TO_GET_RESULTS_FOR,
-//   'key=' + webpageTest_API,
-// ]
+const query2 = [
+  'url=' + URL_TO_GET_RESULTS_FOR,
+  'k=' + webpageTest_API,
+].join('&');
+
 class App extends Component {
 
   state = {
@@ -27,8 +32,9 @@ class App extends Component {
 
   componentDidMount() {
     //Google pageSpeed API
-    fetch(API_URL+query,{
+    fetch(webpageTest_URL+query2,{
       headers: {
+          'Access-Control-Allow-Origin': '*',
           'Content-type':'application/json',
           'Accept':'application/json',
           'cache-control': 'max-age=604800'   //设置浏览器缓存7天
